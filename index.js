9bfd9c07756f5bad5f68d921fe06b480a98f737b
raul reymundi <kinskispiral@gmail.com>
2022-05-03 16:57:11 -0400

    commit tree 893989c46acef8699e0234e476b9631cef765ed7

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return `This one is on me!`
+  }
+    else if (someNumber > 400 && someNumber < 2000) {
+      return `That will be twenty bucks.`;
+    }
+      else if (someNumber > 2000 && someNumber < 2500) {
+        return `I will gladly take your thirty bucks.`;
+      }
+        else if (someNumber > 2500) {
+          return `No can do.`;
+        }
+  }
+
+function ternaryCheckCity(city){
+  const cityGreeting = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityGreeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case 'generous':
+        return  `Thank you so much.`;
+        break;
+    case 'not as generous':
+        return `Thank you.`;
+        break;
+    default:
+        return `Bye.`;
+        break;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
